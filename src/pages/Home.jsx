701609df3585fc854f4bdce4f60c5e02609643b1
betigirma <beti.3170.bassist@gmail.com>
2024-10-22 15:43:25 +0300

    image folder changed to public folder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Home = () => {
   return (
     <div className="p-4">
@@ -8,7 +9,8 @@ const Home = () => {
         Browse our shop to find the best deals on a variety of products.
       </p>
       <img
-        src="../src/assets/images/shopping.jpg" // Shopping-related image
+        src="/assets/images/shopping.jpg"
+ // Shopping-related image
         alt="Shopping"
         className="mt-4 w-full h-auto"
       />
